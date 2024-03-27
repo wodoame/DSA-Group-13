790195c590cec246ac2797640a9ba57d2f288fb7
Wodoame <bmwodoame@gmail.com>
2024-03-27 07:20:41 +0000

    added a sidebar and worked on search passengers

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ class ListNode{
 }
 
 // This is the linked list class which helps us to traverse through the nodes
+// The linked list has been modified to suit the project
 class LinkedList{
     constructor(){
         this.firstNode = undefined;
@@ -166,31 +167,50 @@ let seatNumber = appSettings.seatNumber; // get the current available seat numbe
 const reservationTemplate = {};
 
 
+const reservationSuccessful = ()=>{
+    document.getElementById("success-button").click();
+};
 
+const validateForm = (form) => {
+    let isValid = true;
+    for (let i = 0; i < form.elements.length; i++) {
+        if (!form.elements[i].checkValidity()) {
+            isValid = false;
+            break;
+        }
+    }
+    return isValid;
+};
 const pushDetails = () => {
-    const name = document.getElementById("name").value; // gets the value of the input field with name 'id'
-    const phone = document.getElementById("phone-number").value;
-    const email = document.getElementById("email-address").value; 
-    const dateOfBirth = document.getElementById("date-of-birth").value;
-    const reservations = [];
-    // console.log(name, phone, email);
+    const form = document.getElementById("reservation-form");
+    if(validateForm(form)){
+        const name = document.getElementById("name").value; // gets the value of the input field with name 'id'
+        const phone = document.getElementById("phone-number").value;
+        const email = document.getElementById("email-address").value; 
+        const dateOfBirth = document.getElementById("date-of-birth").value;
+        const reservations = [];
+        // console.log(name, phone, email);
 
-    const reservation = {...reservationTemplate}; 
-    reservation.name = name; 
-    reservation.phone = phone; 
-    reservation.email = email;
-    reservation.displayName = name + ': ' + email
-    reservation.seatNumber = seatNumber++;
-    reservation.dateOfBirth = dateOfBirth;
-    reservation.id = `${Math.floor(Math.random() * 100)}-${reservation.seatNumber}`;
-    reservations.push(reservation);
+        const reservation = {...reservationTemplate}; 
+        reservation.name = name; 
+        reservation.phone = phone; 
+        reservation.email = email;
+        reservation.displayName = name + ': ' + email
+        reservation.seatNumber = seatNumber++;
+        reservation.dateOfBirth = dateOfBirth;
+        reservation.dateBooked = new Date(); 
+        reservation.id = `${Math.floor(Math.random() * 100)}-${reservation.seatNumber}`;
+        reservations.push(reservation);
+        
+        let storedReservations = getReservations(); // get previously stored reservations
+        // If there are stored reservations we parse the array of reservations otherwise we intialize an empty array
+        storedReservations.push(reservation); 
+        localStorage.setItem("app-reservations", JSON.stringify(storedReservations)); // adding the current reservations to the local storage
+        appSettings.seatNumber = seatNumber; // updating available seat number for next passenger
+        saveSettings(appSettings); 
+        reservationSuccessful();
+    }else{document.getElementById('false-submit').click();}
     
-    let storedReservations = getReservations(); // get previously stored reservations
-    // If there are stored reservations we parse the array of reservations otherwise we intialize an empty array
-    storedReservations.push(reservation); 
-    localStorage.setItem("app-reservations", JSON.stringify(storedReservations)); // adding the current reservations to the local storage
-    appSettings.seatNumber = seatNumber; // updating available seat number for next passenger
-    saveSettings(appSettings); 
 }
 
 
@@ -210,13 +230,15 @@ if(showPassengerList(appSettings)){
     storedReservations.forEach((res)=>{reservations.push(res);}); // putting all reservations into the linked list so that they can be ordered (names)
     
     // filling the table with the passenger details
-    reservations.forEach((node)=>{
+    reservations.forEach((node, index)=>{
         const reservation = node.data;
         const htmlContent = `
         <tr>
-            <td class="tw-border-s">${reservation.name}</td>
+            <td class="fw-bold tw-border-s">${index + 1}</td>
+            <td>${reservation.name}</td>
             <td>${reservation.email}</td>
             <td>${reservation.seatNumber}</td>
+            <td>${reservation.dateBooked.slice(0, 10)}</td>
         </tr>
         `;
         tbody.insertAdjacentHTML("beforeend", htmlContent);
@@ -225,3 +247,93 @@ if(showPassengerList(appSettings)){
     appSettings.displayPassengers = false; // resetting this value so that this block of code doesn't run on every page
     saveSettings(appSettings); 
 }
+
+const searchReservations = () => {
+    const displayLocation = document.getElementById("display-location"); 
+    displayLocation.innerHTML = " "; // clear previous search results
+    const query = document.getElementById("search-input").value; // query from the search input
+    const filter = "name"; // filter we are searching by (name, email, id, ....)
+    const matches = new LinkedList(); // new linked list to collect matches
+    const storedReservations = getReservations();
+    storedReservations.forEach((res)=>{
+        if(res[filter].toLowerCase().includes(query.toLowerCase())){
+            matches.push(res);
+        }
+    });
+    matches.display();
+
+    matches.forEach((res, index)=>{
+        const htmlContent = `
+        <li class="bg-white p-3 shadow-sm mb-2 d-flex gap-3">
+            <span class="text-muted" style="font-style:italic;">${index + 1}</span>
+            <span class="fw-bold">${res.value}</span>
+        </li>
+        `;
+       
+        displayLocation.insertAdjacentHTML("beforeend", htmlContent);
+    });
+}
+
+
+const cancelReservation = () => {
+
+};
+
+
+// sidebar 
+const toggle = () => {
+    document.getElementById('sidebar').classList.toggle('close');
+}
+
+const generateSidebar = () => {
+    const sidebarLocation = document.getElementById("sidebar"); 
+    const htmlContent = `
+    <header>
+    <div class="toggle" onclick="toggle()">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:#fff;"><path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path></svg>
+    </div>
+    </header>
+
+    <menu class="mt-5">
+        <ul class="menu-links p-0">
+            <li class="nav-link">
+                <a href="index.html">
+                    <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="m21.743 12.331-9-10c-.379-.422-1.107-.422-1.486 0l-9 10a.998.998 0 0 0-.17 1.076c.16.361.518.593.913.593h2v7a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-4h4v4a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-7h2a.998.998 0 0 0 .743-1.669z"></path></svg>                   
+                    </div>
+                    <div class="text">Home</div>
+                </a>
+            </li>
+            <li class="nav-link">
+                <a href="">
+                    <div class="icon">
+                    <svg fill="#000000" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>cancel</title> <path d="M10.771 8.518c-1.144 0.215-2.83 2.171-2.086 2.915l4.573 4.571-4.573 4.571c-0.915 0.915 1.829 3.656 2.744 2.742l4.573-4.571 4.573 4.571c0.915 0.915 3.658-1.829 2.744-2.742l-4.573-4.571 4.573-4.571c0.915-0.915-1.829-3.656-2.744-2.742l-4.573 4.571-4.573-4.571c-0.173-0.171-0.394-0.223-0.657-0.173v0zM16 1c-8.285 0-15 6.716-15 15s6.715 15 15 15 15-6.716 15-15-6.715-15-15-15zM16 4.75c6.213 0 11.25 5.037 11.25 11.25s-5.037 11.25-11.25 11.25-11.25-5.037-11.25-11.25c0.001-6.213 5.037-11.25 11.25-11.25z"></path> </g></svg>                    
+                        </div>
+                    <div class="text text-nowrap">Cancel Reservations</div>
+                </a>
+            </li>
+            <li class="nav-link">
+                <a href="search-reservation.html">
+                    <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path></svg>                   
+                        </div>
+                    <div class="text">Search</div>
+                </a>
+            </li>
+            <li class="nav-link">
+                <a href="display-passengers.html">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9.5 12c2.206 0 4-1.794 4-4s-1.794-4-4-4-4 1.794-4 4 1.794 4 4 4zm1.5 1H8c-3.309 0-6 2.691-6 6v1h15v-1c0-3.309-2.691-6-6-6z"></path><path d="M16.604 11.048a5.67 5.67 0 0 0 .751-3.44c-.179-1.784-1.175-3.361-2.803-4.44l-1.105 1.666c1.119.742 1.8 1.799 1.918 2.974a3.693 3.693 0 0 1-1.072 2.986l-1.192 1.192 1.618.475C18.951 13.701 19 17.957 19 18h2c0-1.789-.956-5.285-4.396-6.952z"></path></svg>
+                    </div>
+                    <div class="text text-nowrap">Display Passengers</div>
+                </a>
+            </li>
+        
+        </ul>
+    </menu>
+    `;
+
+    sidebarLocation.insertAdjacentHTML("beforeend", htmlContent);
+};
+
+generateSidebar();
